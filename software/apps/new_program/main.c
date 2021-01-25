348bdfe2218a3168ab90fcf0a5fd146c20a2bac9
sgh185 <sgh185@sgh185s-MacBook-Pro.local>
2021-01-24 23:32:42 -0600

    lab1: add skeleton for new app

---
+++ software/apps/new_program/main.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include "nrf.h"
+#include "nrf_delay.h"
+#include "nrf_gpio.h"
+
+// Pin definitions
+#include "nrf52840dk.h"
+
+int main(void) 
+{
+    return 0;
+}
+
