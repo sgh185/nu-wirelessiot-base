56d0e382c24ba847824c02c0bcdddf8b206a684b
sgh185 <sgh185@sgh185s-MacBook-Pro.local>
2021-02-26 12:37:38 -0600

    project: change dummy name to Nordic_Blinky

--- software/apps/project_firmware/sensor_node/main.c
+++ software/apps/project_firmware/sensor_node/main.c
@@ -21,7 +21,7 @@ static simple_ble_config_t ble_config = {
   // c0:98:e5:4e:xx:xx
   .platform_id       = 0x4E,    // used as 4th octect in device BLE address
   .device_id         = 0xAABB,
-  .adv_name          = "CS397/497", // used in advertisements if there is room
+  .adv_name          = "Nordic_Blinky", // used in advertisements if there is room
   .adv_interval      = MSEC_TO_UNITS(1000, UNIT_0_625_MS),
   .min_conn_interval = MSEC_TO_UNITS(500, UNIT_1_25_MS),
   .max_conn_interval = MSEC_TO_UNITS(1000, UNIT_1_25_MS),
