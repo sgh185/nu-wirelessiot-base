c03ee364565f06744643c9a00408a1ba5694a363
sgh185 <sgh185@sgh185s-MacBook-Pro.local>
2021-03-10 05:45:34 -0600

    utils: update config headers

--- software/apps/project_firmware/utils/config.h
+++ software/apps/project_firmware/utils/config.h
@@ -7,10 +7,16 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#include "app_scheduler.h"
+#include "app_timer.h"
 #include "nrf.h"
 #include "nrf_delay.h"
 #include "nrf_gpio.h"
 #include "nrfx_gpiote.h"
+#include "nrf_power.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+
 #include "simple_ble.h"
 #include "nrf52840dk.h"
 
